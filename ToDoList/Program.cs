c2dc16ee3c19a8c3741995f085f1b69f4de3d83b
Dani Renner <danijrenner@gmail.com>
2021-03-02 09:33:43 -0800

    add web UI
    Co-authored-by: Ash Porter <porter.ashley13@gmail.com>

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,56 +1,20 @@
-using System;
-using System.Collections.Generic;
-using ToDoList.Models;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
 namespace ToDoList
 {
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to the To Do List.");
-      Console.WriteLine("Would you like to add an item to your list or view your list? (type add, view or remove)");
-      string userInput = Console.ReadLine();
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-      if (userInput == "add")
-      {
-        Console.WriteLine("Please enter the description for the new item:");
-        string userItem = Console.ReadLine();
-        Item newItem1 = new Item(userItem);
-        Main();
-      }
-      else if (userInput == "remove")
-      {
-        List<Item> result = Item.GetAll();
-        Console.WriteLine("Please type the item description that you want to remove. Please type exact");
-        string userRemove = Console.ReadLine();
-        Item removedItem = null;
-        foreach (Item thisItem in result)
-        {
-          if (thisItem.Description == userRemove)
-          {
-            removedItem = thisItem;
-          }
-        }
-        result.Remove(removedItem);
-        Main();
-      }
-      else if (userInput == "view")
-      {
-        List<Item> result = Item.GetAll();
-        if (result.Count == 0)
-        {
-          Console.WriteLine("You haven't added anything to your To-Do List");
-          Main();
-        }
-        else
-        {
-          foreach (Item thisItem in result)
-          {
-            Console.WriteLine(thisItem.Description);
-          }
-        }
-      }
+      host.Run();
     }
   }
 }
