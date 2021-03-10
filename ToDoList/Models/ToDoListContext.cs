0af014b1638860ca7cfd9b906cc1c1858dbf8352
Dani Renner <danijrenner@gmail.com>
2021-03-10 07:57:39 -0800

    add categories DbSet

--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -7,5 +7,9 @@ namespace ToDoList.Models
     public DbSet<Item> Items { get; set; }
 
     public ToDoListContext(DbContextOptions options) : base(options) { }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
   }
 }
